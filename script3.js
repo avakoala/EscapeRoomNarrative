01922db2c8f289e1d0b4279008188f45d3a79bbc
avakoala <89263808+avakoala@users.noreply.github.com>
2021-10-22 13:35:00 -0400

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,11 @@
+// this is for puzzle 3 password box// 
+
+function checkPassword() {
+   var password = document.getElementById("passwordBox");
+   var passwordText = password.value;
+   if(passwordText == "01001101 01010101 01010011 01001001 0100011" {
+    return true;
+   }
+   alert("Access denied! Incorrect password!");
+   return false;
+   }
