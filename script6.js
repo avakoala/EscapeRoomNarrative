bc967b4198abbe7fb0192034d1e40fc2443a8760
avakoala <89263808+avakoala@users.noreply.github.com>
2021-10-27 12:46:32 -0400

    Update script6.js

--- script6.js
+++ script6.js
@@ -16,7 +16,7 @@ var btn = document.getElementById("myBtn");
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+// Get the image and insert it inside the modal 
 var btn = document.getElementById("myBtn");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
